89c44b2d49f6b193dff631815ca684329f2002f6
AbdimatinAbdullahi <133138873+AbdimatinAbdullahi@users.noreply.github.com>
2025-01-28 21:41:25 +0300

    Revised Tree

--- DataStructures/Trees.py
+++ DataStructures/Trees.py
@@ -1,107 +1,51 @@
 class Node:
     def __init__(self, value):
         self.value = value
+        self.left = None
         self.right = None
-        self.left  = None
 
 class Tree:
     def __init__(self):
         self.root = None
-    
+
     def insert(self, value):
-        if self.root is None:
-            self.root = Node(value)
+        if self.root == None:
+            self.root = node = Node(value)
         else:
             self.insert_recursive(self.root, value)
-    
-    def insert_recursive(self, current, value):
-        if value < current.value:
-            if current.left is None:
-                current.left = Node(value)
-            else:
-                self.insert_recursive(current.left, value)
-        
-        elif value > current.value:
-            if current.right is None:
-                current.right = Node(value)
-            else:
-                self.insert_recursive(current.right, value)
-        else:
-            print("The value exist already in the tree")
-    
-    
-    
-    # Inorder Traversal: left -> root -> right 
-    def in_order_traversal(self):
-        return self.in_order_recursive(self.root, result=[])
 
-    def in_order_recursive(self, root, result=[]):
+    def insert_recursive(self, root, value):
+        if value < root.value:
+            if root.left is None:
+                root.left = node = Node(value)
+            else:
+                self.insert_recursive(root.left, value)
+
+        elif value > root.value:
+            if root.right is None:
+                root.right = node = Node(value)
+            else:
+                self.insert_recursive(root.right, value)
+        else:
+            return "Value already exist in Tree"
+    def Inorder(self):
+        return self.inorder_recursive(self.root, result=[])
+    
+
+    def inorder_recursive(self, root, result):
         if root:
-            self.in_order_recursive(root.left, result)
+            self.inorder_recursive(root.left, result)
             result.append(root.value)
-            self.in_order_recursive(root.right, result)
+            self.inorder_recursive(root.right, result)
         return result
-    
-    
-    
-    # Pre order Traversal: root -> left -> right
-    def pre_order(self):
-        return self.pre_order_recursive(self.root, results =[])
-    
-    def pre_order_recursive(self, root, results=[]):
-        if root:
-            results.append(root.value)
-            self.pre_order_recursive(root.left, results)
-            self.pre_order_recursive(root.right, results)
-        return results
-    
-    
-    
-    # Post Order Traversal: left -> right -> root
-    def post_order(self):
-        return self.post_order_recursive(self.root, results =[])
-    
-    def post_order_recursive(self, root, results=[]):
-        if root:
-            self.post_order_recursive(root.left, results)
-            self.post_order_recursive(root.right, results)
-            results.append(root.value)
-        return results
-    
-
-
-    def search(self, value):
-        if self.root.value == value:
-            return True
-        else:
-            return self.search_recursive(self.root, value)
-    
-    def search_recursive(self, currentNode, value):
-        if currentNode is None:
-            return False
-        elif currentNode.value == value:
-            return True
-        elif currentNode.value < value:
-            return self.search_recursive(currentNode.right, value)
-        else :
-            return self.search_recursive(currentNode.left, value)
-
-
-
-
-
-
-
 
 
 tr = Tree()
-tr.insert(2)
-tr.insert(1)
-tr.insert(0)
 tr.insert(4)
+tr.insert(1)
+tr.insert(6)
+tr.insert(2)
 tr.insert(5)
-tr.insert(1.5)
-print(tr.in_order_traversal())
-print(tr.pre_order())
-print(tr.post_order())
-print(tr.search(-1))
+tr.insert(0)
+tr.insert(7)
+print(tr.Inorder())
