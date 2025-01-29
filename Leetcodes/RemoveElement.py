501be9171618c5c8911557be69fc3867174fc0ea
AbdimatinAbdullahi <133138873+AbdimatinAbdullahi@users.noreply.github.com>
2025-01-29 10:10:23 +0300

    Remove occurences of a value in Array in-place

---
+++ Leetcodes/RemoveElement.py
@@ -0,0 +1,13 @@
+def RemoveElement(nums, val):
+    i = 0
+
+    for j in range(len(nums)):
+        if nums[j] != val:
+            nums[i] = val
+            i += 1
+        else:
+            continue
+    return i
+
+nums = [3, 2, 2, 3]
+print(RemoveElement(nums, 3))
